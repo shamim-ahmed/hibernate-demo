68f8f5bfe972bec783ed116f4296d494380e1036
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-03-26 23:56:29 +0600

    changed the type of id from long to Long

--- hsqldb-intro/src/main/java/edu/buet/cse/hibernate/model/Motd.java
+++ hsqldb-intro/src/main/java/edu/buet/cse/hibernate/model/Motd.java
@@ -4,23 +4,23 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class Motd {
-  private long id;
+  private Long id;
   private String message;
   
   protected Motd() {
   }
   
-  public Motd(long id, String message) {
+  public Motd(Long id, String message) {
 	this.id = id;
 	this.message = message;
   }
 
-  public long getId() {
+  public Long getId() {
 	return id;
   }
 
-  public void setId(long messageId) {
-	this.id = messageId;
+  public void setId(Long id) {
+	this.id = id;
   }
 
   public String getMessage() {
