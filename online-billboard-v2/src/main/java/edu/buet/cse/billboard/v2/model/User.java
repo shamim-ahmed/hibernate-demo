c647496cd2adf18453d2781b159f6da4885df10c
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-08 23:15:23 +0600

    specified the name attribute in User as unique

--- online-billboard-v2/src/main/java/edu/buet/cse/billboard/v2/model/User.java
+++ online-billboard-v2/src/main/java/edu/buet/cse/billboard/v2/model/User.java
@@ -18,7 +18,7 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   
-  @Column(name = "name", nullable = false)
+  @Column(name = "name", nullable = false, unique = true)
   private String name;
   
   @Column(name = "password", nullable = false)
