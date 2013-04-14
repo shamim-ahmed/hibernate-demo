263185e10e128c6c08ffd0f43399134058c18343
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-14 20:31:41 +0600

    added examples of projection and pattern matching using HQL

--- hibernate-ch08/src/main/java/edu/buet/cse/hibernate/ch08/App3.java
+++ hibernate-ch08/src/main/java/edu/buet/cse/hibernate/ch08/App3.java
@@ -6,23 +6,20 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import edu.buet.cse.hibernate.ch08.model.Product;
 import edu.buet.cse.hibernate.ch08.util.HibernateUtil;
 
-// example of using a fully qualified class name in HQL, along with regular expressions
-public class App2 {
+// Example of projection using HQL
+public class App3 {
   public static void main(String... args) {
     try {
       Session session = HibernateUtil.getSession();
-      
       Transaction tx = session.beginTransaction();
-      String hql = "from edu.buet.cse.hibernate.ch08.model.Product as p where p.name like 'M%'";
-      Query query = session.createQuery(hql);
+      Query query = session.createQuery("select p.name, p.price from Product p where p.price <= 25.00");
       @SuppressWarnings("unchecked")
-      List<Product> products = query.list();
+      List<Object[]> results = query.list();
       
-      for (Product p : products) {
-        System.out.println(p);
+      for (Object[] props : results) {
+        System.out.printf("name = %s, price = %s%n", props[0], props[1]);
       }
       
       tx.commit();
