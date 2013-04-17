d2323790e53412d6af963b51fb146bd85ad4f968
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-17 12:30:25 +0600

    added an example on the usage of order by clause in hql

---
+++ hibernate-ch08/src/main/java/edu/buet/cse/hibernate/ch08/App9.java
@@ -0,0 +1,32 @@
+package edu.buet.cse.hibernate.ch08;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.buet.cse.hibernate.ch08.model.Product;
+import edu.buet.cse.hibernate.ch08.util.HibernateUtil;
+
+public class App9 {
+  public static void main(String... args) {
+    try {
+      Session session = HibernateUtil.getSession();
+      
+      Transaction tx = session.beginTransaction();
+      String hql = "from Product p where p.price <= 50.0 order by p.price";
+      Query query = session.createQuery(hql);
+      @SuppressWarnings("unchecked")
+      List<Product> products = query.list();
+      
+      for (Product p : products) {
+        System.out.println(p);
+      }
+      
+      tx.commit();
+    } finally {
+      HibernateUtil.cleanUp();
+    }
+  }
+}
