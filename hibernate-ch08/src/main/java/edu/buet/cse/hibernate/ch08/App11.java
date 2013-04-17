d1dbdcc953090560d93a87311fe547a3da4ee39b
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-17 12:57:10 +0600

    added another example of inner join using HQL

---
+++ hibernate-ch08/src/main/java/edu/buet/cse/hibernate/ch08/App11.java
@@ -0,0 +1,38 @@
+package edu.buet.cse.hibernate.ch08;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.buet.cse.hibernate.ch08.model.Product;
+import edu.buet.cse.hibernate.ch08.model.Supplier;
+import edu.buet.cse.hibernate.ch08.util.HibernateUtil;
+
+public class App11 {
+  public static void main(String... args) {
+    try {
+      Session session = HibernateUtil.getSession();
+      Transaction tx = session.beginTransaction();
+      String hql = "from Product p inner join p.supplier as s";
+      Query query = session.createQuery(hql);
+      @SuppressWarnings("unchecked")
+      List<Object[]> results = query.list();
+      
+      for (Object[] row : results) {
+        Product p = (Product) row[0];
+        Supplier s = (Supplier) row[1];
+        
+        System.out.println(p);
+        System.out.println(s);
+        
+        System.out.print("\n\n");
+      }
+      
+      tx.commit();
+    } finally {
+      HibernateUtil.cleanUp();
+    }
+  }
+}
